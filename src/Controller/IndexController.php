68a734dd834ef65b5ef348036b09689a6177e071
Gustavo L. Martinez <gustavomartinez@gmail.com>
2018-02-04 18:36:10 -0300

    Add Theme https://startbootstrap.com/template-overviews/creative/

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -14,6 +14,8 @@ class IndexController extends Controller
     public function index()
     {
         // replace this line with your own code!
-        return $this->render('@Maker/demoPage.html.twig', [ 'path' => str_replace($this->getParameter('kernel.project_dir').'/', '', __FILE__) ]);
+        return $this->render('index.html.twig', [ 'path' => str_replace($this->getParameter('kernel.project_dir').'/', '', __FILE__) ]);
     }
+
+
 }
