c94bf97356dbc9da80fae2ec581de534fd823bfe
Gustavo L. Martinez <gustavomartinez@gmail.com>
2018-02-14 20:29:11 -0300

    Add web server and doctrine fixtures

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,6 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
 ];
